6401b17bab97230f4c2e03bc47775411f7e1d92c
Duessmann <gabriel.duessmann@gmail.com>
2021-09-11 14:16:00 -0300

    init

---
+++ src/main/java/com/vocabulary/vocabularymetricsbackend/VocabularyMetricsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vocabulary.vocabularymetricsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyMetricsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularyMetricsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vocabulary/vocabularymetricsbackend/VocabularyMetricsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocabulary.vocabularymetricsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyMetricsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
